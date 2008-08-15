446fe5a60ad906db82c00ab4e847c42e35313c9f
jacereda <jacereda@localhost>
2008-08-15 22:21:33 +0000

    Added test for multiple windows

---
+++ test/multi.c
@@ -0,0 +1,59 @@
+#include <aw/sysgl.h>
+#include <aw/aw.h>
+#include "log.h"
+
+static int g_exit = 0;
+
+static void processEvents(aw * w) {
+	const awEvent * awe;
+	while ((awe = awNextEvent(w))) switch (awe->type) {
+	case AW_EVENT_RESIZE:
+		Log("Resized to %d %d", awe->u.resize.w, awe->u.resize.h); break;
+	case AW_EVENT_DOWN:
+		Log("Down: %d", awe->u.down.which); break;
+	case AW_EVENT_UP:
+		Log("Up: %d", awe->u.up.which); break;
+	case AW_EVENT_MOTION:
+		Log("Motion: %d,%d", awe->u.motion.x, awe->u.motion.y); break;
+	case AW_EVENT_CLOSE:
+		Log("Exit requested");
+		g_exit = 1; break;
+	}
+}
+
+#define NWIN 16
+
+static void draw(int n) {
+	float f = (float)n / NWIN;
+	glClearColor(0, 1 - f, f, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+static void handle(aw * w, int n) {
+	awPushCurrent(w);
+	draw(n);
+	awSwapBuffers(w);
+	processEvents(w);
+	awPopCurrent(w);
+}
+
+int main(int argc, char ** argv) {
+	int i;
+	aw * w[NWIN];
+	awInit();
+	for (i = 0; i < NWIN; i++)
+		w[i] = awOpen(argv[0], 100 + 16*i, 100+16*i, 300, 400);
+	while (!g_exit) 
+		for (i = 0; i < NWIN; i++) 
+			handle(w[i], i);
+	for (i = 0; i < NWIN; i++) 
+		awClose(w[i]);
+	awEnd();
+	return 0;
+}
+
+/* 
+Local variables: **
+c-file-style: "bsd" **
+End: **
+*/
