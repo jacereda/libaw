f54171040fbfcfe1aee39e9d587ffc502236cb5a
jacereda <jacereda@localhost>
2009-05-31 19:43:05 +0000

    License headers

--- aw.h
+++ aw.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2008, Jorge Acereda Maciá
+  Copyright (c) 2008-2009, Jorge Acereda Maciá
   All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
