cbd0e99a77ce5a1238207c3017eb3da2f77d5019
jacereda <jacereda@localhost>
2010-06-06 15:40:29 +0000

    More iphone bits

--- sysgl.h
+++ sysgl.h
@@ -1,9 +1,10 @@
-#if !defined(__APPLE__) || AWBACKEND != cocoa
-#if defined(_WIN32)
+#if !defined __APPLE__ || AWBACKEND != cocoa
+#if defined _WIN32
 #include <windows.h>
 #endif
 #include <GL/gl.h>
+#elif defined __IPHONE_OS_VERSION_MIN_REQUIRED
+#include <OpenGLES/ES1/gl.h>
 #else
 #include <OpenGL/gl.h>
 #endif
-
