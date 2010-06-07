a27665b05e4e8dd43875bb629658f7d68ea9dd21
jacereda <jacereda@localhost>
2010-06-07 22:42:30 +0000

    plist template for NPAPI plugins

--- awplugin.r
+++
@@ -1,5 +0,0 @@
-#include <CoreServices/CoreServices.r>
-
-resource 'STR#' (126, "Description") { { "AW plugin", "AW plugin" } };
-resource 'STR#' (127, "MIME type description") { { "AW plugin" } };
-resource 'STR#' (128, "MIME type") { { "application/awplugin" } };
