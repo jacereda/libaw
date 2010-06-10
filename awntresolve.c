94c5d5ab0c93800e15289973eb27d4225f5bb3c1
jacereda <jacereda@localhost>
2010-06-10 23:55:36 +0000

    Fix Windows build

--- awntresolve.c
+++ awntresolve.c
@@ -1,9 +1,18 @@
 #include <windows.h>
 
 void * awosSelf() {
-	return GetModuleHandle("npawplugin.dll"); // XXX
+	HMODULE ret;
+	GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, 
+			  (LPCSTR)awosSelf,  &ret);
+	return ret;
 }
 
 void * awosResolve(void * in, const char * name) {
 	return GetProcAddress(in, name);
+}
+
+const char * awosModPath() {
+	static char buf[256];
+	GetModuleFileName(awosSelf(), buf, sizeof(buf));
+	return buf;
 }
